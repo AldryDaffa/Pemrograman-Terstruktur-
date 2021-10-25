4849bb67734dd0ae52d175e7fa3d58f68940633e
Aldry Daffa Rukmansyah <89761151+AldryDaffa@users.noreply.github.com>
2021-10-25 12:59:09 +0700

    Delete Chapter 5 directory

--- Chapter 5/Pratikum5_Latihan1.1.py
+++
@@ -1,9 +0,0 @@
-bindo = int(input("Masukkan nilai Bhs Indonesia : "))
-ipa   = int(input("Masukkan nilai IPA           : "))
-mat   = int(input("Masukkan nilai Matematika    : "))
-
-
-if(bindo >= 60) and (mat >= 60) and (mat > 70):
-    print("LULUS")
-else:
-    print("TIDAK LULUS")
--- Chapter 5/Pratikum5_Latihan2.1.py
+++
@@ -1,5 +0,0 @@
-angka = 0
-while(angka <= 100):
-    if(angka % 2 == 1):
-        print(angka)
-    angka += 1
