b56bddb7d7fb468e900b25ae779538a2542beb07
Aldry Daffa Rukmansyah <89761151+AldryDaffa@users.noreply.github.com>
2021-10-25 12:58:58 +0700

    Delete Chapter 4 directory

--- Chapter 4/Pratikum4_Latihan5.py
+++
@@ -1,4 +0,0 @@
-mhslaki = int(input("jumlah mahasiswa laki-laki PTIK21?"))
-mhsperempuan = int(input("jumlah mahasiswa perempuan PTIK21?"))
-print("Laki-Laki =" + int(mhslaki/10)*"|")
-print("Perempuan =" + int(mhsperempuan/10)*"|")
