2ec22c6dafd590490f1eebf8205c1aeebbb18de2
Aldry Daffa Rukmansyah <89761151+AldryDaffa@users.noreply.github.com>
2021-12-07 19:26:13 +0700

    Delete Latihan 1.py

--- Latihan 1.py
+++
@@ -1,9 +0,0 @@
-def diffDate(x):
-    import datetime
-    skrg = datetime.datetime.now()
-    from datetime import datetime
-    awal = datetime.strptime(x, "%Y-%m-%d")
-    rms = awal - skrg
-    print(abs(rms.days))
-
-diffDate("2020-12-30")
